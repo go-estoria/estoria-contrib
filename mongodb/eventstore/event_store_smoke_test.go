291b239d6c24f275c992d3ce4c32f72acec30383
Jeff Linse <jeff@jefflinse.com>
2025-02-10 20:59:45 -0800

    Add smoke test case for each strategy

--- mongodb/eventstore/event_store_smoke_test.go
+++ mongodb/eventstore/event_store_smoke_test.go
@@ -5,7 +5,10 @@ import (
 	"testing"
 
 	"github.com/go-estoria/estoria-contrib/mongodb/eventstore"
+	"github.com/go-estoria/estoria-contrib/mongodb/eventstore/strategy"
 	"github.com/go-estoria/estoria-contrib/tests"
+	"github.com/go-estoria/estoria/typeid"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 func TestEventStore_SmokeTest(t *testing.T) {
@@ -24,14 +27,55 @@ func TestEventStore_SmokeTest(t *testing.T) {
 
 	for _, tt := range []struct {
 		name     string
-		haveOpts func(*testing.T) []eventstore.EventStoreOption
+		haveOpts func(*testing.T, *mongo.Database) []eventstore.EventStoreOption
 	}{
 		{
-			name:     "store with default options",
-			haveOpts: func(*testing.T) []eventstore.EventStoreOption { return []eventstore.EventStoreOption{} },
+			name: "store with default options",
+			haveOpts: func(*testing.T, *mongo.Database) []eventstore.EventStoreOption {
+				return []eventstore.EventStoreOption{}
+			},
+		},
+		{
+			name: "store with single collection strategy",
+			haveOpts: func(t *testing.T, db *mongo.Database) []eventstore.EventStoreOption {
+				t.Helper()
+				collection := db.Collection("events")
+				t.Cleanup(func() {
+					if err := collection.Drop(ctx); err != nil {
+						t.Fatalf("tc cleanup: failed to drop collection: %v", err)
+					}
+				})
+				strat, err := strategy.NewSingleCollectionStrategy(mongoClient, collection)
+				if err != nil {
+					t.Fatalf("tc setup: failed to create SingleCollectionStrategy: %v", err)
+				}
+				return []eventstore.EventStoreOption{eventstore.WithStrategy(strat)}
+			},
+		},
+		{
+			name: "store with multi collection strategy",
+			haveOpts: func(t *testing.T, db *mongo.Database) []eventstore.EventStoreOption {
+				t.Helper()
+				strat, err := strategy.NewMultiCollectionStrategy(mongoClient, db,
+					strategy.CollectionSelectorFunc(func(typeid.UUID) string {
+						return "events"
+					}),
+				)
+				if err != nil {
+					t.Fatalf("tc setup: failed to create MultiCollectionStrategy: %v", err)
+				}
+				return []eventstore.EventStoreOption{eventstore.WithStrategy(strat)}
+			},
 		},
 	} {
-		eventStore, err := eventstore.New(mongoClient, tt.haveOpts(t)...)
+		database := mongoClient.Database("estoria")
+		t.Cleanup(func() {
+			if err := database.Drop(ctx); err != nil {
+				t.Fatalf("tc cleanup: failed to drop database: %v", err)
+			}
+		})
+
+		eventStore, err := eventstore.New(mongoClient, tt.haveOpts(t, database)...)
 		if err != nil {
 			t.Fatalf("tc setup: failed to create EventStore: %v", err)
 		}
